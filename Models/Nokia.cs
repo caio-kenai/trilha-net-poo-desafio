e2f8f52612dda48165a00db4144d860d3a906d9f
Caio Pacifico <caiokenai10@gmail.com>
2025-08-04 21:21:33 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,22 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void AboutPhone()
+        {
+            Console.WriteLine($"Seu celular é da marca Nokia tem o modelo: {ShowModelo()}, tem o imei de {ShowImei()} e tem {ShowMemoria()} de memoria ram");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no celular Nokia");
+        }
+
     }
 }
