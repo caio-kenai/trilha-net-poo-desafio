a748d1add8deab290c00513a98ecc14e1784b3df
Caio Pacifico <caiokenai10@gmail.com>
2025-08-04 21:21:43 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,16 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string modelo;
+        private string imei;
+        private int memoria;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
@@ -20,7 +25,21 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Recebendo ligação...");
         }
+        public string ShowModelo()
+        {
+            return modelo;
+        }
 
+        public string ShowImei()
+        {
+            return imei;
+        }
+        public int ShowMemoria()
+        {
+            return memoria;
+        }
+
+        public abstract void AboutPhone();
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
