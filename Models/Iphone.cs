1bf597ed9dc270d54e654edb8c3c457b3001d00f
Caio Pacifico <caiokenai10@gmail.com>
2025-08-04 21:21:22 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void AboutPhone()
+        {
+            Console.WriteLine($"Seu celular é da marca Iphone tem o modelo: {ShowModelo()}, tem o imei de {ShowImei()} e tem {ShowMemoria()} de memoria ram");
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no celular Iphone");
+        }
     }
 }
