005615e367b6fa4900e89419df64067a7d70c94c
Caio Pacifico <caiokenai10@gmail.com>
2025-08-04 21:22:05 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Nokia nokia = new(numero:"9959883181",modelo:"Modelo 1",imei:"4390742",memoria:12);
+nokia.InstalarAplicativo("YouTube");
+nokia.AboutPhone();
+Console.WriteLine("------------------------------------------------------------------------------------------------");
+Console.WriteLine("iphone:");
+Iphone iphone = new(numero:"963494991",modelo: "Modelo X",imei: "432434",memoria: 10);
+iphone.InstalarAplicativo("Twitter");
+iphone.AboutPhone();
